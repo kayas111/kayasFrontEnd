6137fffd74e56f198ca8334175229f5e8b55be2a
kayas111 <onongeisaac@gmail.com>
2026-06-23 14:16:16 +0300

    added three quick access buttons to home page

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import { MessageComponent } from './Functions'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
 export function Homepage(){
     return(
@@ -9,10 +10,25 @@ export function Homepage(){
                <div class="col-md-3"></div>
                <div class="col-md-6">
                
-             <div style={{paddingTop:"120px"}}>  
+             <div style={{paddingTop:"80px"}}>  
                 
-             <div class="pageLabel">Welcome</div><p></p>
-               
+             <div style={{textAlign:"center"}}>
+                <div style={{padding:"20px"}}><div class="pageLabel" style={{textAlign:"center"}}>Welcome</div>
+             <div class='light'>Select your choice</div></div>
+          
+            
+             
+             </div>
+
+<div class="flexDisplayWithGap" style={{justifyContent:"center"}}>
+
+<Link to={'/pages/hostels/hostelslist'}><div class="btn btn-sm btn-success">Hostels</div></Link>
+<Link to={'/pages/pubarticles/sharemyarticles/773367078'}><div class="btn btn-sm btn-warning">Vacists 2026</div></Link>
+<Link to={'/pages/attendanceregs/myregisters'}><div class="btn btn-sm btn-success">Bulk SMS</div></Link>
+
+
+</div>
+             <p></p>
                <MessageComponent  message="Use the menu at the top to explore products and services. Incase a page is not responsive, it means it is undergoing maintenance"/>
                {/* <MessageComponent  message="Tap menu or look at the menu above and select an option"/> */}
                 </div>
