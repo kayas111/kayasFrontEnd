4094613465bbdb9a06aa17c42c95cc649464b184
kayas111 <onongeisaac@gmail.com>
2026-09-21 16:48:44 +0300

    added posters

--- src/pages/makererePosters/MakererePosters.js
+++ src/pages/makererePosters/MakererePosters.js
@@ -1,17 +1,42 @@
 import { MessageComponent } from "../Functions"
 
-import mp1 from './makererePostersImgs/mp1.jpg'
+import mp1 from './makererePostersImgs/mp1.jpeg'
 import mp2 from './makererePostersImgs/mp2.jpg'
 import mp3 from './makererePostersImgs/mp3.jpg'
+import mp4 from './makererePostersImgs/mp4.jpg'
+import mp5 from './makererePostersImgs/mp5.jpeg'
+import mp6 from './makererePostersImgs/mp6.jpeg'
+import mp7 from './makererePostersImgs/mp7.jpeg'
+import mp8 from './makererePostersImgs/mp8.jpeg'
+import mp9 from './makererePostersImgs/mp9.jpeg'
+import mp10 from './makererePostersImgs/mp10.jpeg'
+import mp11 from './makererePostersImgs/mp11.jpeg'
+import mp12 from './makererePostersImgs/mp12.jpeg'
+import mp13 from './makererePostersImgs/mp13.jpeg'
+import mp14 from './makererePostersImgs/mp14.jpeg'
+import { Link } from "react-router-dom/cjs/react-router-dom.min"
 
 
 export function MakererePosters(){
 
 
 let makererePosters=[
-    {src:mp1,text:"Moses Ssebuliba - CEDAT"},
+   
+  
+    {src:mp7},
+    {src:mp8},
+    {src:mp9},
+    {src:mp1},
+    {src:mp4},
     {src:mp3},
-    {src:mp2}
+    {src:mp2},
+    {src:mp10},
+    {src:mp11},
+    {src:mp12},
+    {src:mp13},
+    {src:mp5},
+    {src:mp6},
+    {src:mp14}
     
 ]
 
@@ -27,6 +52,13 @@ return(<>
             })()}</div>
             <div class="pageDescription">Updates are made daily. Keep visiting this page to stay updated.</div>
             
+            <p></p>
+            <div class="flexDisplayWithGap">
+<Link to={'/pages/hostels/hostelslist'}><div class="btn btn-sm btn-success">Hostels</div></Link>
+<Link to={'/pages/pubarticles/sharemyarticles/773367078'}><div class="btn btn-sm btn-success">Makerere updates</div></Link>
+
+
+            </div>
             <p></p>
 
 {(()=>{
